62197e6d3ed28113c76a86b97819e682c1bbdfaf
Omar Farag <omarfarag74@gmail.com>
2021-02-21 04:40:05 -0500

    Delete actions.js

--- _layouts/actions.js
+++
@@ -1,9 +0,0 @@
-//Expands button when clicked on
-function openClose(y) {
-  var x = document.getElementById(y);
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
