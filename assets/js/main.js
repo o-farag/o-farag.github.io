04abd816d2b9347cc11c96b6ef2fbc72d1f49d8e
Omar Farag <omarfarag74@gmail.com>
2021-06-14 02:20:28 -0400

    Added mobile navbar

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+/* Show menu*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {navMenu.classList.add('show-menu')});
+}
+
+/* Hide menu */
+
+if (navClose) {
+    navClose.addEventListener('click', () => {navMenu.classList.remove('show-menu')});
+}
+
+/*Remove mobile menu*/
+const navLink = document.querySelectorAll('.nav_link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
