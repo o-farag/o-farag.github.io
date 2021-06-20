39993ff7eecc36c28aa83229da3d28daca97c128
Omar Farag <omarfarag74@gmail.com>
2021-06-20 01:32:49 -0400

    Added education and work history section

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,25 @@ function toggleSkills () {
 }
 
 skillsHeader.forEach((el) => {el.addEventListener('click', toggleSkills)});
+
+/* Qualification tabs */
+
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target)
+    
+    tabContents.forEach(tabContent => {
+        tabContent.classList.remove('qualification_active')
+    })
+    target.classList.add('qualification_active')
+
+    tabs.forEach(tab => {
+        tab.classList.remove('qualification_active')
+    })
+    tab.classList.add('qualification_active')
+    })
+
+})
